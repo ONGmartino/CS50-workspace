0dfc346ded8f1a857491772eae0dbc33f5ca7990
Martino <martino.ongaro@gmail.com>
2016-06-17 06:56:40 +0000

    pset7 concluded

--- pset7/pset7.sql
+++ pset7/pset7.sql
@@ -1,35 +1,110 @@
+-- MySQL dump 10.13  Distrib 5.5.46, for debian-linux-gnu (x86_64)
 --
--- Database: `pset7`
+-- Host: 0.0.0.0    Database: pset7
+-- ------------------------------------------------------
+-- Server version	5.5.46-0ubuntu0.14.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `history`
 --
 
-CREATE DATABASE IF NOT EXISTS `pset7`;
-USE `pset7`;
+DROP TABLE IF EXISTS `history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `history` (
+  `type` char(1) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `symbol` varchar(20) NOT NULL,
+  `shares` int(11) NOT NULL,
+  `price` decimal(10,4) NOT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`date`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `history`
+--
+
+LOCK TABLES `history` WRITE;
+/*!40000 ALTER TABLE `history` DISABLE KEYS */;
+INSERT INTO `history` VALUES ('b','2016-06-05 10:44:53','msft',1,51.7900,16),('b','2016-06-05 10:48:26','msft',20,51.7900,16),('b','2016-06-05 10:51:15','msft',10,51.7900,16),('s','2016-06-05 10:51:23','msft',12,51.7900,16),('b','2016-06-05 11:05:31','aapl',10,97.9200,16),('b','2016-06-05 11:07:41','amzn',3,725.5400,16),('s','2016-06-05 11:08:03','aapl',10,97.9200,16),('s','2016-06-05 11:10:39','amzn',3,725.5400,16),('b','2016-06-05 12:06:25','aapl',10,97.9200,17),('b','2016-06-05 12:06:35','amzn',3,725.5400,17),('s','2016-06-05 12:06:56','aapl',1,97.9200,17),('b','2016-06-14 08:35:54','fb',10,113.9500,18),('b','2016-06-14 08:36:09','msft',10,50.1400,18),('s','2016-06-14 08:36:44','msft',5,50.1400,18),('d','2016-06-15 07:36:00','',0,654.0000,16),('d','2016-06-15 07:49:31','',0,100000.0000,16),('s','2016-06-15 07:49:49','aapl',0,97.4600,16),('b','2016-06-15 07:49:56','fb',654,114.9400,16),('s','2016-06-15 07:55:53','msft',0,49.8300,16);
+/*!40000 ALTER TABLE `history` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `portfolios`
+--
+
+DROP TABLE IF EXISTS `portfolios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `portfolios` (
+  `id` int(11) NOT NULL,
+  `symbol` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` decimal(10,4) NOT NULL,
+  `shares` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`symbol`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `portfolios`
+--
+
+LOCK TABLES `portfolios` WRITE;
+/*!40000 ALTER TABLE `portfolios` DISABLE KEYS */;
+INSERT INTO `portfolios` VALUES (16,'fb','Facebook, Inc.',114.9400,654),(16,'msft','Microsoft Corporation',51.7900,18),(17,'aapl','Apple Inc.',97.9200,9),(17,'amzn','Amazon.com, Inc.',725.5400,3),(17,'MSFT','Microsoft Corporation',51.7900,9),(18,'fb','Facebook, Inc.',113.9500,10),(18,'msft','Microsoft Corporation',50.1400,5);
+/*!40000 ALTER TABLE `portfolios` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `hash` varchar(255) NOT NULL,
+  `cash` decimal(65,4) unsigned NOT NULL DEFAULT '0.0000',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB;
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (username, hash) VALUES('andi', '$2y$10$c.e4DK7pVyLT.stmHxgAleWq4yViMmkwKz3x8XCo4b/u3r8g5OTnS');
-INSERT INTO `users` (username, hash) VALUES('caesar', '$2y$10$0p2dlmu6HnhzEMf9UaUIfuaQP7tFVDMxgFcVs0irhGqhOxt6hJFaa');
-INSERT INTO `users` (username, hash) VALUES('eli', '$2y$10$COO6EnTVrCPCEddZyWeEJeH9qPCwPkCS0jJpusNiru.XpRN6Jf7HW');
-INSERT INTO `users` (username, hash) VALUES('hdan', '$2y$10$o9a4ZoHqVkVHSno6j.k34.wC.qzgeQTBHiwa3rpnLq7j2PlPJHo1G');
-INSERT INTO `users` (username, hash) VALUES('jason', '$2y$10$ci2zwcWLJmSSqyhCnHKUF.AjoysFMvlIb1w4zfmCS7/WaOrmBnLNe');
-INSERT INTO `users` (username, hash) VALUES('john', '$2y$10$dy.LVhWgoxIQHAgfCStWietGdJCPjnNrxKNRs5twGcMrQvAPPIxSy');
-INSERT INTO `users` (username, hash) VALUES('levatich', '$2y$10$fBfk7L/QFiplffZdo6etM.096pt4Oyz2imLSp5s8HUAykdLXaz6MK');
-INSERT INTO `users` (username, hash) VALUES('rob', '$2y$10$3pRWcBbGdAdzdDiVVybKSeFq6C50g80zyPRAxcsh2t5UnwAkG.I.2');
-INSERT INTO `users` (username, hash) VALUES('skroob', '$2y$10$395b7wODm.o2N7W5UZSXXuXwrC0OtFB17w4VjPnCIn/nvv9e4XUQK');
-INSERT INTO `users` (username, hash) VALUES('zamyla', '$2y$10$UOaRF0LGOaeHG4oaEkfO4O7vfI34B1W23WqHr9zCpXL68hfQsS3/e');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'andi','$2y$10$c.e4DK7pVyLT.stmHxgAleWq4yViMmkwKz3x8XCo4b/u3r8g5OTnS',10000.0000),(2,'caesar','$2y$10$0p2dlmu6HnhzEMf9UaUIfuaQP7tFVDMxgFcVs0irhGqhOxt6hJFaa',10000.0000),(3,'eli','$2y$10$COO6EnTVrCPCEddZyWeEJeH9qPCwPkCS0jJpusNiru.XpRN6Jf7HW',10000.0000),(4,'hdan','$2y$10$o9a4ZoHqVkVHSno6j.k34.wC.qzgeQTBHiwa3rpnLq7j2PlPJHo1G',10000.0000),(5,'jason','$2y$10$ci2zwcWLJmSSqyhCnHKUF.AjoysFMvlIb1w4zfmCS7/WaOrmBnLNe',10000.0000),(6,'john','$2y$10$dy.LVhWgoxIQHAgfCStWietGdJCPjnNrxKNRs5twGcMrQvAPPIxSy',10000.0000),(7,'levatich','$2y$10$fBfk7L/QFiplffZdo6etM.096pt4Oyz2imLSp5s8HUAykdLXaz6MK',10000.0000),(8,'rob','$2y$10$3pRWcBbGdAdzdDiVVybKSeFq6C50g80zyPRAxcsh2t5UnwAkG.I.2',10000.0000),(9,'skroob','$2y$10$395b7wODm.o2N7W5UZSXXuXwrC0OtFB17w4VjPnCIn/nvv9e4XUQK',10000.0000),(10,'zamyla','$2y$10$UOaRF0LGOaeHG4oaEkfO4O7vfI34B1W23WqHr9zCpXL68hfQsS3/e',10000.0000),(16,'marti','$2y$10$waSa/Cb7i79npOKo/06aqOE93Qp7wPsUKaD9oWrfhk1R26d1Wk632',34654.6000),(17,'alba','$2y$10$ZauV1gWfpNe43rOnH81DA.IYbKDGxsRW3CsQK6T23lsWWPrSOBDIy',6475.9900),(18,'morales','$2y$10$.u1QN88NSRZR4SToAnvT1eAXkIHSHsXYxND7zoUYe6cuvZMm3VlBO',8609.8000);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-16  7:35:14
